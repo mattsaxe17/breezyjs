678f838a83bba4e5e0e2fbc03cfb8b8daf720d3c
Matthew Saxe <mattsaxe17@gmail.com>
2021-06-16 21:26:35 -0400

    Implement number error handling

--- src/methods/number/index.js
+++ src/methods/number/index.js
@@ -1,5 +1,10 @@
+import { throwGenericError, typeCheckArgs, typeCheckSpreadArgs, requirePositiveNumbers, requireWholeNumbers, genericErrorCheck } from '../../error/helpers';
+
 // Bounds a number to greater than or equal to min and less than or equal to max
 Number.prototype.clamp = function (min, max) {
+  typeCheckArgs('clamp', arguments, ['number', 'number']);
+  genericErrorCheck(max <= min, 'clamp', `Argument at index 0 ("min") must be less than argument at index 1 ("max")`);
+
   if (this < min) return min;
   if (this > max) return max;
   return this;
@@ -7,6 +12,10 @@ Number.prototype.clamp = function (min, max) {
 
 // Returns a number rounded to the nearest multiple of the provided step value
 Number.prototype.round = function (step) {
+  typeCheckArgs('round', arguments, ['number']);
+  requirePositiveNumbers('round', arguments, [0]);
+
+
   let decimalPlaces;
   let str = step.toString();
 
@@ -21,6 +30,8 @@ Number.prototype.round = function (step) {
 
 // Loops to invoke a function n times; i is available
 Number.prototype.times = function (func) {
+  typeCheckArgs('times', arguments, ['function']);
+
   for (let i = 0; i < this; i++) {
     func(i);
   }
--- src/methods/window/index.js
+++ src/methods/window/index.js
@@ -7,8 +7,9 @@ window.loop = function (times, callback) {
   return arr;
 }
 
-window.random = function (low, high, decimalPlaces) {
-  if (arguments.length == 1) return Math.floor(Math.random() * arguments[0]);
+window.random = function (low = 0, high = 1, decimalPlaces = 10) {
+  if (arguments.length === 1) return Math.floor(Math.random() * arguments[0]);
+
   return parseFloat((Math.random() * (high - low) + low).toFixed(decimalPlaces));
 }
 
